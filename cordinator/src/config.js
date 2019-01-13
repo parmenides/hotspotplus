8c58b4ae30ae4976cd156cd900e7f8c13604efb2
Payam <payam.yousefi@bamilo.com>
2019-01-13 23:42:41 +0330

    add .env and fix config server url

--- cordinator/src/config.js
+++ cordinator/src/config.js
@@ -1,6 +1,6 @@
 //var CONFIG_SERVER_URL = "https://lcapi.prohotspotplus.com/api";
 //var CONFIG_SERVER_URL = "http://lcapi.captiveportal.ir/api";
-var CONFIG_SERVER_URL = 'http://127.0.0.1:4000/api';
+var CONFIG_SERVER_URL = 'http://hotspotpluslicensapi:3000/api';
 
 module.exports = {
   CONFIG_SERVER_URL: CONFIG_SERVER_URL,
