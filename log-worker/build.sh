c3fbb246d3f55dc7a474eafe647d65a28e059404
Payam Yousefi <root.p.y@gmail.com>
2019-02-09 20:40:11 +0330

    release

---
+++ log-worker/build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -e
+
+REGISTRY="registry.gitlab.com"
+
+get_latest_commit(){
+    echo $(git log -n 1 --pretty=format:""%H)
+}
+
+build_image(){
+    docker build -t ${CI_REGISTRY_IMAGE}/logaggregator:latest -t ${CI_REGISTRY_IMAGE}/logaggregator:${CI_COMMIT_TAG} -f ./log-aggregator/Dockerfile.build ./log-aggregator
+    docker push ${CI_REGISTRY_IMAGE}/logaggregator:${CI_COMMIT_TAG}
+    docker push ${CI_REGISTRY_IMAGE}/logaggregator:latest
+}
+
+build_image
