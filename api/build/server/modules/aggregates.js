d769c7eb842cbf09bc72c622999b55277a7602ac
Payam Yousefi <root.p.y@gmail.com>
2019-04-04 15:12:46 +0430

    releaseForSaas

--- api/build/server/modules/aggregates.js
+++ api/build/server/modules/aggregates.js
@@ -4,8 +4,8 @@ var log = logger.createLogger()
 
 const ACCOUNTING_INDEX = `${process.env.ELASTIC_INDEX_PREFIX}accounting`
 const CHARGE_INDEX = `${process.env.ELASTIC_INDEX_PREFIX}charge`
+const LICENSE_CHARGE_INDEX = `${process.env.ELASTIC_INDEX_PREFIX}licensecharge`
 const {Client} = require('@elastic/elasticsearch')
-
 const elasticClient = new Client({
   node: `http://${process.env.ELASTIC_IP}:${process.env.ELASTIC_PORT}`,
   apiVersion: '6.7',
