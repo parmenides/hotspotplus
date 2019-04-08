3acb326e49af3601ad5d1b225ad4dce56ca25a8d
Payam Yousefi <root.p.y@gmail.com>
2019-04-08 23:37:22 +0430

    fix

--- log-worker/dist/controllers/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const controller = {
-    health: (request, response) => {
-        response.send({ ok: true });
-    },
-};
-exports.default = controller;
-//# sourceMappingURL=index.js.map
--- log-worker/dist/enum/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var IP;
-(function (IP) {
-    IP["IP"] = "internal";
-})(IP = exports.IP || (exports.IP = {}));
-//# sourceMappingURL=index.js.map
