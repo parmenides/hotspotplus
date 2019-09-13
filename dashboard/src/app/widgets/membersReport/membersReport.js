330d92b5de47f81e6e27282f571a6a201f74be0b
Payam Yousefi <root.p.y@gmail.com>
2019-09-13 16:05:03 +0430

    fix chart

--- dashboard/src/app/widgets/membersReport/membersReport.js
+++ dashboard/src/app/widgets/membersReport/membersReport.js
@@ -12,7 +12,7 @@ app.directive('membersReport', [
   'translateNumberFilter',
   'appMessenger',
   'trimUsernameFilter',
-  function(
+  function (
     PREFIX,
     $log,
     $rootScope,
@@ -28,112 +28,43 @@ app.directive('membersReport', [
       scope: {
         params: '=options'
       },
-      controller: function($scope) {
-        $scope.localLang = $rootScope.localLang;
-        $scope.direction = $rootScope.direction;
-        $scope.loading = false;
-        makeTable();
-        $scope.$on($scope.params.reloadEvent, function(event, data) {
+      controller: function ($scope) {
+        $scope.localLang = $rootScope.localLang
+        $scope.direction = $rootScope.direction
+        $scope.loading = false
+        $scope.members = []
+        getPage()
+        $scope.$on($scope.params.reloadEvent, function (event, data) {
           if (data.params.advanceTime) {
-            $scope.params.fromDate = data.params.advanceTime.startDate;
+            $scope.params.fromDate = data.params.advanceTime.startDate
           } else {
-            $scope.params.fromDate = data.params.fromDate;
+            $scope.params.fromDate = data.params.fromDate
           }
-          makeTable();
-        });
-        function makeTable() {
+          getPage()
+        })
+        function getPage() {
+          $scope.loading = true
+          var options = {}
+          options.id = $scope.params.businessId
+          options.filter = {}
+          options.filter.sort = 'creationDate DESC'
+          options.filter.skip = 0
+          options.filter.limit = 10
+          options.filter.fields = {internetPlanHistory: false}
 
-          $scope.paginationOptions = {
-            pageNumber: 1,
-            itemPerPage: 10,
-            sort: null
-          };
-
-          $scope.gridOptions = {
-            enableSorting: false,
-            enablePaginationControls: false,
-            paginationPageSize: $scope.paginationOptions.itemPerPage,
-            enableRowSelection: false,
-            enableSelectAll: false,
-            showGridFooter: false,
-            enableColumnResizing: false,
-            columnDefs: [
-              {
-                displayName: 'member.username',
-                field: 'username',
-                enableHiding: false,
-                enableSorting: false,
-                enableColumnMenu: false,
-                headerCellFilter: 'translate',
-                cellFilter: 'cleanupUsername',
-              },
-              {
-                displayName: 'member.creationDate',
-                field: 'creationDate',
-                enableHiding: false,
-                enableSorting: false,
-                enableColumnMenu: false,
-                headerCellFilter: 'translate',
-                cellTemplate: '<div class="ui-grid-cell-contents" ng-if="row.entity.creationDate">{{row.entity.creationDate |  translateDate | translateNumber }}{{"general.,"| translate}}&nbsp;{{"general.hour"| translate}}:&nbsp;{{row.entity.creationDate |  date : "HH:mm" | translateNumber }}</div>'
-              },
-              {
-                displayName: 'member.internetPlanName',
-                field: 'internetPlanName',
-                width: 150,
-                enableHiding: false,
-                enableSorting: false,
-                enableColumnMenu: false,
-                headerCellFilter: 'translate',
-                cellTemplate:
-                  '<div class="ui-grid-cell-contents">{{row.entity.internetPlanName}}</div>'
-              },
-              {
-                displayName: 'member.status',
-                field: 'active',
-                width: 150,
-                enableHiding: false,
-                enableSorting: false,
-                enableColumnMenu: false,
-                headerCellFilter: 'translate',
-                cellTemplate:
-                  '<div class="ui-grid-cell-contents" ng-if="row.entity.active">{{"member.active"|translate}}</div>' +
-                  '<div class="ui-grid-cell-contents" ng-if="!row.entity.active">{{"member.inActive"|translate}}</div>'
-              },
-            ],
-            onRegisterApi: function(gridApi) {
-              $scope.gridApi = gridApi;
-              getPage();
+          Business.members(options).$promise.then(
+            function (members) {
+              $scope.members = members
+              $scope.loading = false
+            },
+            function (error) {
+              $scope.loading = true
+              $log.error(error)
             }
-          };
-
-          $scope.pageChanges = function() {
-            $scope.loading = true;
-            getPage();
-          };
-
-          var getPage = function() {
-            var options = {};
-            options.id = $scope.params.businessId;
-            options.filter = {};
-            options.filter.sort = 'creationDate DESC';
-            options.filter.skip = 0
-            options.filter.limit = 10;
-            options.filter.fields = { internetPlanHistory: false };
-
-            Business.members(options).$promise.then(
-              function(members) {
-                $scope.gridOptions.data = members;
-                $scope.loading = false;
-              },
-              function(error) {
-                $scope.loading = true;
-                $log.error(error);
-              }
-            );
-          };
+          )
         }
       },
       templateUrl: PREFIX + 'app/widgets/membersReport/tpl/membersReport.html'
-    };
+    }
   }
-]);
+])
