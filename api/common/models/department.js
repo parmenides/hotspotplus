ae811865bd40c0c02b76273a4ac41def71338331
Payam Yousefi <root.p.y@gmail.com>
2020-01-03 10:42:44 +0330

    add remove dependency in branches

--- api/common/models/department.js
+++ api/common/models/department.js
@@ -1,5 +1,28 @@
-'use strict';
+'use strict'
+const app = require('../../server/server')
+const logger = require('../../server/modules/logger')
 
-module.exports = function(Department) {
+module.exports = function (Department) {
+  const log = logger.createLogger()
 
-};
+  Department.observe('before delete', async function (ctx) {
+    const Member = app.models.Member
+    const instance = ctx.instance
+    log.debug({instance})
+    const affectedMembers = await Member.find({
+      where: {
+        departments: {
+          eq: instance.id.toString()
+        }
+      }
+    })
+    if (affectedMembers.length > 0) {
+      const error = new Error()
+      error.message = 'has related members'
+      error.status = 400
+      throw error
+    }
+    return
+  })
+
+}
